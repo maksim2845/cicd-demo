c34b3904d1a188891901a40007297c4ccf65a6f6
maksim2845 <maksimvarenov07@gmail.com>
2026-05-27 08:47:22 +0000

    Fix linting issues in bash script and yaml config

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 echo "Starting deployment at: $(date)"
-# Специально используем переменную без кавычек, чтобы ShellCheck выдал предупреждение
-echo "Deploy user is: " $USER
+echo "Deploy user is: "$USER"
 ls -la
